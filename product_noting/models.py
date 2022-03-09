781fd7c61aae03a2e1928aa5a29cada1e9f99fc9
abhirajdas06 <90388893+abhirajdas06@users.noreply.github.com>
2022-03-09 16:52:38 +0530

    main

--- product_noting/models.py
+++ product_noting/models.py
@@ -13,5 +13,5 @@ class Position(models.Model):
 class Employee(models.Model):
     fullname = models.CharField(max_length=100)
     emp_code = models.CharField(max_length=3)
-    mobile = models.CharField(max_length=12
+    mobile = models.CharField(max_length=12)
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
